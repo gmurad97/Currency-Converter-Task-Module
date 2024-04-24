a180668f36f0748befdfc31e6447b17829d9d0fe
gmurad97 <murad.dev@bk.ru>
2024-04-24 23:22:36 +0400

    script (without code optimization)

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,6 +1,6 @@
 const API_URL = "https://api.freecurrencyapi.com/v1/latest?apikey=fca_live_eI9vCmHvEFCXiVfhcbN24WpaQAAnz6dKouZw4qsE";
-const FROM_CURRENCY_DEFAULT_STATE = "USD";
-const TO_CURRENCY_DEFAULT_STATE = "RUB";
+let FROM_CURRENCY_DEFAULT_STATE = "USD";
+let TO_CURRENCY_DEFAULT_STATE = "RUB";
 const FROM_CURRENCY_DEFAULT_VALUE = 1;
 const FLOAT_FIXED_VALUE = 5;
 
@@ -15,7 +15,19 @@ document.addEventListener("DOMContentLoaded", function () {
     const FROM_REPORT_CURRENT_RATE = document.querySelector("#from_report_current_rate");
     const TO_REPORT_CURRENT_RATE = document.querySelector("#to_report_current_rate");
 
+    FROM_REPORT_VALUE.value = FROM_CURRENCY_DEFAULT_VALUE;
 
+    FROM_REPORT_VALUE.addEventListener("input", function () {
+        getCurrencyRate(FROM_CURRENCY_DEFAULT_STATE, TO_CURRENCY_DEFAULT_STATE).then((response) => {
+            TO_REPORT_VALUE.value = (response * parseFloat(FROM_REPORT_VALUE.value));
+        });
+    });
+
+    TO_REPORT_VALUE.addEventListener("input", function () {
+        getCurrencyRate(TO_CURRENCY_DEFAULT_STATE, FROM_CURRENCY_DEFAULT_STATE).then((response) => {
+            FROM_REPORT_VALUE.value = (response * parseFloat(TO_REPORT_VALUE.value));
+        });
+    });
 
     FROM_CURRENCY_BUTTONS.forEach((from_button) => {
         if (from_button.getAttribute("data-from-currency") === FROM_CURRENCY_DEFAULT_STATE.toLowerCase()) {
@@ -26,6 +38,11 @@ document.addEventListener("DOMContentLoaded", function () {
                 from_btn.classList.remove("converter__from-menu-btn--active");
             });
             this.classList.add("converter__from-menu-btn--active");
+            updateReportRate();
+            FROM_CURRENCY_DEFAULT_STATE = this.getAttribute("data-from-currency").toUpperCase();
+            getCurrencyRate(FROM_CURRENCY_DEFAULT_STATE, TO_CURRENCY_DEFAULT_STATE).then((response) => {
+                TO_REPORT_VALUE.value = response;
+            });
         });
     });
 
@@ -38,8 +55,32 @@ document.addEventListener("DOMContentLoaded", function () {
                 to_btn.classList.remove("converter__to-menu-btn--active");
             });
             this.classList.add("converter__to-menu-btn--active");
+            updateReportRate();
+
+            TO_CURRENCY_DEFAULT_STATE = this.getAttribute("data-to-currency").toUpperCase();
+            getCurrencyRate(FROM_CURRENCY_DEFAULT_STATE, TO_CURRENCY_DEFAULT_STATE).then((response) => {
+                TO_REPORT_VALUE.value = response;
+            });
+
+
+
+
         });
     });
+
+
+    function updateReportRate() {
+
+        getCurrencyRate(FROM_CURRENCY_DEFAULT_STATE, TO_CURRENCY_DEFAULT_STATE).then((response) => {
+            FROM_REPORT_CURRENT_RATE.textContent = `1 ${FROM_CURRENCY_DEFAULT_STATE} = ${response} ${TO_CURRENCY_DEFAULT_STATE}`;
+        });
+
+        getCurrencyRate(TO_CURRENCY_DEFAULT_STATE, FROM_CURRENCY_DEFAULT_STATE).then((response) => {
+            TO_REPORT_CURRENT_RATE.textContent = `1 ${TO_CURRENCY_DEFAULT_STATE} = ${response} ${FROM_CURRENCY_DEFAULT_STATE}`;
+
+        });
+
+    }
 
     async function getCurrencyRate(from_currency = FROM_CURRENCY_DEFAULT_STATE, to_currency = TO_CURRENCY_DEFAULT_STATE) {
         try {
@@ -60,7 +101,6 @@ document.addEventListener("DOMContentLoaded", function () {
 
     getCurrencyRate(FROM_CURRENCY_DEFAULT_STATE, TO_CURRENCY_DEFAULT_STATE).then((response) => {
         TO_REPORT_VALUE.value = response;
+        updateReportRate();
     });
-
-    f
 });
